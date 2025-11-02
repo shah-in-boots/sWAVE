c45001dcd09075cdeee59950df90129dfd10e8a5
Anish S. Shah <6852177+shah-in-boots@users.noreply.github.com>
2025-11-02 15:00:07 -0700

    Refine waveform viewer dependencies and plot

--- R/mod_waveform_viewer.R
+++ R/mod_waveform_viewer.R
@@ -73,7 +73,7 @@ mod_waveform_viewer_server <- function(id, egm, controls) {
         plotData <- plotData[plotData$sample >= start & plotData$sample <= end, , drop = FALSE]
       }
 
-      plotData
+      downsample_waveform(plotData)
     })
 
     output$plot <- plotly::renderPlotly({
@@ -82,40 +82,92 @@ mod_waveform_viewer_server <- function(id, egm, controls) {
 
       channelNames <- unique(data$label)
 
-      g <-
-        data |>
-        ggplot2::ggplot(ggplot2::aes(x = sample, y = mV)) +
-        ggplot2::geom_line(color = "white") +
-        ggplot2::facet_wrap(~ label, ncol = 1, scales = "free_y", strip.position = "left") +
-        EGM::theme_egm_dark()
+      axis_template <- list(
+        color = "rgba(255,255,255,1)",
+        gridcolor = "rgba(255,255,255,0.1)",
+        zerolinecolor = "rgba(255,255,255,0.2)",
+        tickfont = list(color = "rgba(255,255,255,0.9)")
+      )
 
-      p <-
-        g |>
-        plotly::ggplotly(dynamicTicks = TRUE, originalData = TRUE)
+      plots <- lapply(channelNames, function(channel) {
+        channel_data <- data[data$label == channel, , drop = FALSE]
 
-      if (!is.null(p$x$layout$annotations)) {
-        for (i in seq_along(p$x$layout$annotations)) {
-          ann <- p$x$layout$annotations[[i]]
+        plotly::plot_ly(
+          channel_data,
+          x = ~sample,
+          y = ~mV,
+          type = "scatter",
+          mode = "lines",
+          name = channel,
+          line = list(color = "rgba(255,255,255,1)"),
+          hoverinfo = "text",
+          text = ~paste0(
+            "Channel: ", channel,
+            "<br>Sample: ", sample,
+            "<br>mV: ", round(mV, 3)
+          )
+        ) |>
+          plotly::layout(
+            yaxis = c(axis_template, list(title = channel)),
+            xaxis = axis_template
+          )
+      })
 
-          if (ann$text %in% channelNames) {
-            ann$textangle <- 0
-            ann$font$color <- "rgba(255,255,255,1)"
-            ann$xref <- "paper"
-            ann$x <- -0.01
-            ann$xanchor <- "right"
-            ann$yref <- "paper"
-            ann$yanchor <- "middle"
-          }
+      subplot <- plotly::subplot(
+        plots,
+        nrows = length(channelNames),
+        shareX = TRUE,
+        titleX = FALSE,
+        titleY = TRUE,
+        which_layout = "merge",
+        margin = 0.02
+      )
 
-          p$x$layout$annotations[[i]] <- ann
-        }
-      }
-
-      p |>
-        plotly::layout(margin = list(l = 80)) |>
-        plotly::layout(dragmode = "zoom")
+      subplot |>
+        plotly::layout(
+          showlegend = FALSE,
+          dragmode = "zoom",
+          plot_bgcolor = "rgba(17,17,17,1)",
+          paper_bgcolor = "rgba(17,17,17,1)",
+          margin = list(l = 80, r = 20, t = 20, b = 40)
+        ) |>
+        plotly::config(displaylogo = FALSE)
     })
   })
+}
+
+downsample_waveform <- function(data, max_points_per_channel = 5000) {
+  if (!is.data.frame(data)) {
+    return(data)
+  }
+
+  required_cols <- c("label", "sample")
+  if (!all(required_cols %in% names(data))) {
+    return(data)
+  }
+
+  max_points_per_channel <- max(1L, as.integer(max_points_per_channel))
+
+  label_levels <- unique(data$label)
+
+  dt <- data.table::as.data.table(data)
+  data.table::setorderv(dt, c("label", "sample"))
+
+  downsampled <- dt[, {
+    if (.N <= max_points_per_channel) {
+      .SD
+    } else {
+      idx <- unique(as.integer(floor(seq(1, .N, length.out = max_points_per_channel))))
+      .SD[idx]
+    }
+  }, by = "label"]
+
+  downsampled <- as.data.frame(downsampled)
+  downsampled$label <- factor(downsampled$label, levels = label_levels)
+  downsampled <- downsampled[order(downsampled$label, downsampled$sample), , drop = FALSE]
+  downsampled$label <- as.character(downsampled$label)
+
+  downsampled
 }
 
 ## To be copied in the UI
